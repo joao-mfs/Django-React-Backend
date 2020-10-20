e60a583e4c14ea41adbe5b92182a614d721066f0
joao-mfs <jmfelix83@gmail.com>
2020-10-20 14:35:45 -0300

    Final

--- backend/settings.py
+++ backend/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core',
     'rest_framework.authtoken',
+    'core',
     'corsheaders',
 ]
 
--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
 from core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,6 +13,6 @@ router.register(r'item', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
     path('admin/', admin.site.urls),
 ]
